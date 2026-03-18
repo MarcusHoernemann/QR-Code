ae52c7dac358e19d5617db0b61a5ac11e0846563
Marcus Hörnemann <marcushoernemann@gmail.com>
2026-03-18 13:04:07 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,18 @@
-const CACHE_NAME = 'qr-go-v1.7.7';
-const ASSETS = ['./', './index.html', './manifest.json', './logo.png'];
+const CACHE_NAME = 'qr-go-v1.8.0';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'logo.png'
+];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
-  self.clients.claim();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
