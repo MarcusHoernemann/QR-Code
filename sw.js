df1699f728d90b898c261ec10a5a46698a4df34e
Marcus Hörnemann <marcushoernemann@gmail.com>
2026-03-18 12:31:08 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,34 @@
-const CACHE_NAME = 'qr-go-v1.7.2';
+const CACHE_NAME = 'qr-go-v1.7.3';
 const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'logo.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.png'
 ];
 
+// Installieren und Assets in den Cache laden
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
     })
   );
+  self.skipWaiting();
 });
 
+// Aktivieren und alte Caches löschen
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Netzwerk-Anfragen abfangen
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
