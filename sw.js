c1b1ebffe05876e568c1ae3d058a3a43f7ea6fb1
Marcus Hörnemann <marcushoernemann@gmail.com>
2026-03-18 12:34:03 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,14 @@
-const CACHE_NAME = 'qr-go-v1.7.3';
+const CACHE_NAME = 'qr-go-v1.7.4';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './logo.png'
+  'index.html',
+  'manifest.json',
+  'logo.png'
 ];
 
-// Installieren und Assets in den Cache laden
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-// Aktivieren und alte Caches löschen
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-// Netzwerk-Anfragen abfangen
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
